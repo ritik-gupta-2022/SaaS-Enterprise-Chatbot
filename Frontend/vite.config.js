808eeae64a87551857b6110f48fd007724c63ed9
ritik-gupta-2022 <aggupta0936@gmail.com>
2025-01-12 22:20:43 +0530

    Initialize tailwind shadcn

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // Listen on all interfaces
+    port: 5173,      // Ensure the port matches
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
